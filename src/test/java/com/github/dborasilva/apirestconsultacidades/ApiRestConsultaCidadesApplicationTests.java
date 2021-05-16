58d7659c1bab98f87557f7f86691c5f7afcf3a1b
dbora-silva <82063557+dbora-silva@users.noreply.github.com>
2021-05-15 21:24:18 -0300

    Primeiro commit.
    Criação da estrutura do projeto pela ferramenta Spring Initializr.

---
+++ src/test/java/com/github/dborasilva/apirestconsultacidades/ApiRestConsultaCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dborasilva.apirestconsultacidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestConsultaCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
